71743824e3515d4a17117a54ff2329e3df1727df
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2022-08-23 13:14:11 -0700

    added keyboard functionalities
    Now the user can use the commands on the keyboard to play the drumset

--- index.js
+++ index.js
@@ -52,15 +52,62 @@ for(let i=0;i<document.querySelectorAll(".drum").length;i++){
 
 document.addEventListener("keydown", function(event){
 
-  console.log(event);
-  console.log(check(event));
-  alert("Key was pressed");
+  console.log(event); // this is what triggers the event listener
+  // console.log(check(event));
+  // alert(check(event)+" Key was pressed");
+  let key = check(event);
+  switch(key){
+
+    case 'w':
+      let audio1 = new Audio("./sounds/tom-1.mp3");
+      audio1.play();
+      break;
+
+    case 'a':
+      let audio2 = new Audio("./sounds/tom-2.mp3");
+      audio2.play();
+      break;
+
+    case 's':
+      let audio3 = new Audio("./sounds/tom-3.mp3");
+      audio3.play();
+      break;
+
+    case 'd':
+      let audio4 = new Audio("./sounds/tom-4.mp3");
+      audio4.play();
+      break;
+
+    case  'j':
+      let audio5 = new Audio("./sounds/snare.mp3");
+      audio5.play();
+      break;
+
+    case 'k':
+      let audio6 = new Audio("./sounds/kick-bass.mp3");
+      audio6.play();
+      break;
+
+    case 'l':
+      let audio7 = new Audio("./sounds/crash.mp3");
+      audio7.play();
+      break;
+
+    default: console.log("buttonInnerHTML");
+
+
+  }
 
 });
 
+
+
 function check(event){
+
   return event.key;
+
 }
+
 
 // document.addEventListener("keydown", function(event){
 //   alert("key was pressed");
