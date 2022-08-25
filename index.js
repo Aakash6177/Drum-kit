429075171c9e9afcaed3e880122220b2577946e3
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2022-08-25 15:12:03 -0700

    Updated the index.js
    Applied the callback function for the key down

--- index.js
+++ index.js
@@ -1,144 +1,47 @@
 
-
-for(let i=0;i<document.querySelectorAll(".drum").length;i++){
-  document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-
-
-    console.log(this.innerText);
-    let letter = this.innerText;
-    switch(letter){
-
-      case 'w':
-        let audio1 = new Audio("./sounds/tom-1.mp3");
-        audio1.play();
-        break;
-
-      case 'a':
-        let audio2 = new Audio("./sounds/tom-2.mp3");
-        audio2.play();
-        break;
-
-      case 's':
-        let audio3 = new Audio("./sounds/tom-3.mp3");
-        audio3.play();
-        break;
-
-      case 'd':
-        let audio4 = new Audio("./sounds/tom-4.mp3");
-        audio4.play();
-        break;
-
-      case  'j':
-        let audio5 = new Audio("./sounds/snare.mp3");
-        audio5.play();
-        break;
-
-      case 'k':
-        let audio6 = new Audio("./sounds/kick-bass.mp3");
-        audio6.play();
-        break;
-
-      case 'l':
-        let audio7 = new Audio("./sounds/crash.mp3");
-        audio7.play();
-        break;
-
-      default: console.log("buttonInnerHTML");
-
-    }
-
-  });
+function check(event){
+  return event.key;
 }
 
-document.addEventListener("keydown", function(event){
-
-  console.log(event); // this is what triggers the event listener
-  // console.log(check(event));
-  // alert(check(event)+" Key was pressed");
-  let key = check(event);
-  switch(key){
-
-    case 'w':
-      let audio1 = new Audio("./sounds/tom-1.mp3");
+ document.addEventListener('keydown',()=>{
+   let key = check(event);
+   switch(key){
+     case 'w':
+      let audio1 = new Audio('./sounds/tom-1.mp3');
       audio1.play();
       break;
-
-    case 'a':
-      let audio2 = new Audio("./sounds/tom-2.mp3");
+    case 'a' :
+      let audio2 = new Audio('./sounds/tom-2.mp3');
       audio2.play();
       break;
 
     case 's':
-      let audio3 = new Audio("./sounds/tom-3.mp3");
+      let audio3 = new Audio('./sounds/tom-3.mp3');
       audio3.play();
       break;
 
     case 'd':
-      let audio4 = new Audio("./sounds/tom-4.mp3");
+      let audio4 = new Audio('./sounds/tom-4.mp3');
       audio4.play();
       break;
 
-    case  'j':
-      let audio5 = new Audio("./sounds/snare.mp3");
+    case 'j':
+      let audio5 = new Audio('./sounds/snare.mp3');
       audio5.play();
       break;
 
     case 'k':
-      let audio6 = new Audio("./sounds/kick-bass.mp3");
+      let audio6 = new Audio('./sounds/kick-bass.mp3');
       audio6.play();
       break;
 
     case 'l':
-      let audio7 = new Audio("./sounds/crash.mp3");
+      let audio7 = new Audio('./sounds/crash.mp3');
       audio7.play();
       break;
 
-    default: console.log("buttonInnerHTML");
+    default:
+      console.log(check(event));
+   }
 
-
-  }
-
-});
-
-
-
-function check(event){
-
-  return event.key;
-
-}
-
-
-// document.addEventListener("keydown", function(event){
-//   alert("key was pressed");
-// });
-// let result = this.innerHTML;
-
-// if(result === 'w'){
-//   let audio = new Audio("./sounds/tom-1.mp3");
-//   audio.play();
-// }
-
-// else if(result === 'a'){
-//   let audio = new Audio("./sounds/tom-2.mp3");
-//   audio.play();
-// }
-// else if(result === 's'){
-//   let audio = new Audio("./sounds/tom-3.mp3");
-//   audio.play();
-// }
-// else if(result === 'd'){
-//   let audio = new Audio("./sounds/tom-4.mp3");
-//   audio.play();
-// }
-// else if(result === 'j'){
-//   let audio = new Audio("./sounds/snare.mp3");
-//   audio.play();
-// }
-// else if(result === 'k'){
-//   let audio = new Audio("./sounds/kick.mp3");
-//   audio.play();
-// }
-// else if(result === 'j'){
-//   let audio = new Audio("./sounds/crash.mp3");
-//   audio.play();
+})
