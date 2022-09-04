04c15366bd59a2cc03ee8a0124dc1f8013fa926f
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2022-09-04 15:12:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,35 +9,49 @@ function check(event){
      case 'w':
       let audio1 = new Audio('./sounds/tom-1.mp3');
       audio1.play();
+      console.log(check(event));
+      buttonAnimation(key);
       break;
     case 'a' :
       let audio2 = new Audio('./sounds/tom-2.mp3');
       audio2.play();
+      console.log(check(event));
+      buttonAnimation(key);
       break;
 
     case 's':
       let audio3 = new Audio('./sounds/tom-3.mp3');
       audio3.play();
+      console.log(check(event));
+      buttonAnimation(key);
       break;
 
     case 'd':
       let audio4 = new Audio('./sounds/tom-4.mp3');
       audio4.play();
+      console.log(check(event));
+      buttonAnimation(key);
       break;
 
     case 'j':
       let audio5 = new Audio('./sounds/snare.mp3');
       audio5.play();
+      console.log(check(event));
+      buttonAnimation(key);
       break;
 
     case 'k':
       let audio6 = new Audio('./sounds/kick-bass.mp3');
       audio6.play();
+      console.log(check(event));
+      buttonAnimation(key);
       break;
 
     case 'l':
       let audio7 = new Audio('./sounds/crash.mp3');
       audio7.play();
+      console.log(check(event));
+      buttonAnimation(key);
       break;
 
     default:
@@ -45,3 +59,10 @@ function check(event){
    }
 
 })
+
+
+function buttonAnimation(Key){
+  let activeButton = document.querySelector("."+key);
+  console.log(activeButton);
+  // activeButton.classList.add("pressed");
+}
